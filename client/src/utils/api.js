a0655b6e1d5b5a063ee26bccb5373daca0a131e5
xiaohai-huang <jeromehuang18@gmail.com>
2021-09-16 10:42:12 +0800

    Add react dev port env variable

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,6 @@
-const URL = "http://localhost:3000";
+const PORT = process.env.REACT_APP_PORT || 3000;
+const DEV_URL = `http://localhost:${PORT}`;
+const URL = process.env.NODE_ENV === "production" ? "" : DEV_URL;
 
 export const api = (endpoint, config) => {
   const response = fetch(URL + endpoint, config).then((res) => res.json());
